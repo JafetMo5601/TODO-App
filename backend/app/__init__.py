86e1a6b306fa34b736440a56d7ca99ea0a902632
jmo.py <jafet.moraugalde15@gmail.com>
2021-05-17 08:38:16 -0600

    The app was modified to use CORS.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,6 +2,7 @@ from app.build.config import Connections
 from flask import Flask
 from flask_mysqldb import MySQL
 from flask_json import FlaskJSON
+from flask_cors import CORS, cross_origin
 
 import os
 
@@ -12,16 +13,20 @@ __connections = Connections()
 
 def createApp():
     app = Flask(__name__)
+    CORS(app)
     FlaskJSON(app)
     mysql = __connections.dbConfig(app)
+    
     return registerBP(app)
 
 
 def registerBP(app):
     # Pending to add the registration of the endpoints
     from .login import loginBP
-
     app.register_blueprint(loginBP)
+
+    from .tasks_management import tasksBP
+    app.register_blueprint(tasksBP)
 
     return app
 
