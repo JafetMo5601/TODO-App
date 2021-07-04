31aa06c8de848379846360c8f548253237777765
jmo.py <jafet.moraugalde15@gmail.com>
2021-07-04 11:26:20 -0600

    Remove unnecessary lines.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,6 +1,5 @@
 from werkzeug.utils import secure_filename
 from flask import Flask, request, jsonify
-# from flask_login import LoginManager
 from flask_mysqldb import MySQL
 from flask_cors import CORS
 import json
@@ -9,40 +8,38 @@ import os
 from instance.config import app_config
 
 
-# login_manager = LoginManager()
 mysql = MySQL()
 
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app = config_app(app, config_name)
-    # db.init_app(app)
     mysql = MySQL(app)
     CORS(app)
 
-    return app 
+    return app
 
 
 def config_app(app, config):
     app.config.from_object(app_config[config])
-    app.config.from_pyfile('config.py')    
+    app.config.from_pyfile("config.py")
+    app.config["CORS_HEADERS"] = "Content-Type"
     app.config["MYSQL_HOST"] = "localhost"
     app.config["MYSQL_USER"] = "root"
-    app.config["MYSQL_PASSWORD"] = os.getenv('DBPASSWD')
+    app.config["MYSQL_PASSWORD"] = os.getenv("DBPASSWD")
     app.config["MYSQL_DB"] = "todo"
     app = register_bp(app)
 
-    # login_manager.init_app(app)
-    # login_manager.login_message = "You must be logged in to access this page"
-    # login_manager.login_view = "auth.login"
-    
     return app
+
 
 def register_bp(app):
     from app.tasks import tasks_bp
+
     app.register_blueprint(tasks_bp)
-    
+
     from app.admin import admin_bp
+
     app.register_blueprint(admin_bp)
 
     return app
--- backend/entrypoint.py
+++ backend/entrypoint.py
@@ -3,7 +3,7 @@ import os
 from app import create_app
 
 
-config_name = "development"  # = os.environ.get('FLASK_CONFIG')
+config_name = os.environ.get('FLASK_CONFIG')
 app = create_app(config_name)
 
 
